396dc6809003d15171ff9991d4cf988270b0e73c
seungwoo Kim <kyh196201@gmail.com>
2022-08-19 00:44:12 +0900

    App 컴포넌트 불필요한 부분 제거

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,22 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+
 import App from './App';
 
 describe('<App />', () => {
-  it('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-  });
-
   it('renders title', () => {
     const { container } = render(<App />);
 
     expect(container).toHaveTextContent('Wordle');
-  });
-
-  it('renders button', () => {
-    const { getByText } = render(<App />);
-
-    expect(getByText('버튼이에용')).toBeInTheDocument();
   });
 });
