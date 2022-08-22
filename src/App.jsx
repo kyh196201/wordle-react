8849597800952f8097a486cd1c80cab3e66ec9f4
seungwoo Kim <kyh196201@gmail.com>
2022-08-23 00:22:31 +0900

    Style App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import styled from '@emotion/styled';
+
 import Header from '@/components/Header';
+
+const Container = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '100vh',
+});
+
+const Main = styled.main({
+  flex: 1,
+});
 
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
       <Header />
-    </div>
+
+      <Main>게임 영역</Main>
+    </Container>
   );
 }
 
