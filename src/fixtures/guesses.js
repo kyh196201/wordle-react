0f7000c8dc37cc2a5f163da59fafa105aefed76e
seungwoo Kim <kyh196201@gmail.com>
2022-09-08 00:50:54 +0900

    guesses fixture 추가

---
+++ src/fixtures/guesses.js
@@ -0,0 +1,26 @@
+import { STATUS } from '@/constants/settings';
+
+export const GUESS = [
+  {
+    letter: 'a',
+    status: STATUS.CORRECT,
+  },
+  {
+    letter: 'p',
+    status: STATUS.EXIST,
+  },
+  {
+    letter: 'p',
+    status: STATUS.EXIST,
+  },
+  {
+    letter: 'l',
+    status: STATUS.MISS,
+  },
+  {
+    letter: 'e',
+    status: STATUS.CORRECT,
+  },
+];
+
+export default {};
