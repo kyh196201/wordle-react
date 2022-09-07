c9f355744f84971f16ba7ce4da63f89307e485f4
seungwoo Kim <kyh196201@gmail.com>
2022-09-08 00:50:43 +0900

    앱 설정에 필요한 최대 시도 횟수 및 status 관련 상수 변수 추가

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -1,3 +1,11 @@
 export const MAX_WORD_LENGTH = 5;
 
+export const MAX_CHANCES = 6;
+
+export const STATUS = {
+  EXIST: 'exist', // 글자가 존재함
+  CORRECT: 'correct', // 위치, 글자 모두 맞아
+  MISS: 'miss', // 글자가 없음
+};
+
 export default {};
