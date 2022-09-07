698aee09b580327fa319235e45a1b03c12562454
seungwoo Kim <kyh196201@gmail.com>
2022-09-08 00:12:31 +0900

    Board 컴포넌트 조건문 수정
    guesses의 length가 0보다 큰지 체크를 안하니 화면에 텍스트 0이 렌더링되어서
    실제 길이가 0보다 큰지 체크하는 조건을 추가했습니다.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,19 +1,19 @@
 import { MAX_CHANCES } from '@/constants/settings';
 
 export default function Board({ guesses }) {
-  const guessesLength = (guesses || []).length;
+  const guessesLength = (guesses ?? []).length;
   const emptyLength = MAX_CHANCES - guessesLength;
 
   return (
     <div>
       {/* 추측 rows */}
-      {guessesLength &&
+      {guessesLength > 0 &&
         guesses.map((guess, index) => (
           <div key={`guess-${index}`}>{guess}</div>
         ))}
 
       {/* 빈 rows */}
-      {emptyLength &&
+      {emptyLength > 0 &&
         [...Array(emptyLength)].map((v, index) => (
           <div key={index}>{index}</div>
         ))}
