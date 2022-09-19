8aa8a91fcbcfcd4b34c27f3194b58bac4f35104c
seungwoo Kim <kyh196201@gmail.com>
2022-09-19 23:26:46 +0900

    LetterBox 스타일 적용

--- src/components/LetterBox.jsx
+++ src/components/LetterBox.jsx
@@ -1,3 +1,14 @@
+import styled from '@emotion/styled';
+
+const Box = styled.span({
+  display: 'flex',
+  width: '6.2rem',
+  height: '6.2rem',
+  margin: '0 0.5rem 0.5rem 0',
+  border: '1px solid #d3d6da',
+  fontSize: '3.2rem',
+});
+
 export default function LetterBox({ letter = '' }) {
-  return <span>{letter}</span>;
+  return <Box className="letter-box">{letter}</Box>;
 }
